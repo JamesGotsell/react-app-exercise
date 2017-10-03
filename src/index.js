aaf70eefb96ed117691b014e306943e8e46af01b
James Gotsell <james.gotsell@dotter.me>
2017-10-03 21:14:38 +0100

    added configureStore file and comment

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ import injectTapEventPlugin from 'react-tap-event-plugin'
 import Root from './components/Root'
 import './index.css'
 
+// within here in need to import configureStore so i can pass store into root
+
 injectTapEventPlugin()
 
 ReactDOM.render(<Root />, document.getElementById('root'))
